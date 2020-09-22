0af41d40563bb3b6e99044e869a7d8d56439ffb6
Shingirayi Innocent Mandebvu <shingim1@gmail.com>
2020-09-22 16:58:29 +0200

    refactor: fix exports

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import { BooksForm } from './BooksForm';
-import BooksList from './BooksList';
+import BooksForm from '../containers/BooksForm';
+import BooksList from '../containers/BooksList';
 
 function App() {
   return (
     <div className="App">
-      <BooksList />
       <BooksForm />
+      <BooksList />
     </div>
   );
 }
