e795d2400aab42cb68a0c6274dd6aaf60bd189ee
Shingirayi Innocent Mandebvu <shingim1@gmail.com>
2020-09-22 16:57:04 +0200

    refactor: fix reducers

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,15 +1,16 @@
-const CREATE_BOOK = 'CREATE_BOOK';
-const REMOVE_BOOK = 'REMOVE_BOOK';
+import { CREATE_BOOK, REMOVE_BOOK } from '../actions/index';
 
 const booksReducer = (state = [], action) => {
   switch (action.type) {
     case CREATE_BOOK:
-      return [...state, action.book];
+      return { books: [...state.books, action.book] };
     case REMOVE_BOOK:
-      console.log(action.book);
-      return [
-        ...state.slice(0, action.book),
-        ...state.slice(action.book + 1, state.length)];
+      return {
+        books:
+        [...state.books.slice(0, action.book),
+          ...state.books.slice(action.book + 1,
+            state.books.length)],
+      };
     default:
       return state;
   }
